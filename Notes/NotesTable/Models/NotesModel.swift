62aaa6c7b56e7e3419b3cd7601b7a87a875b5d71
Андрей <andrej.tch@yandex.ru>
2025-03-18 23:03:38 +0300

    перенос изменений с ветки dev

--- Notes/NotesTable/Models/NotesModel.swift
+++ Notes/NotesTable/Models/NotesModel.swift
@@ -12,20 +12,31 @@ class NoteModel: ObservableObject, NoteModelLogic {
     
     var notes: [Note] = [] {
         didSet {
+            notesCaretacer.save(values: notes)
             notesSubject.send(notes)
         }
     }
     var notesPublisher: AnyPublisher<[Note], Never> {
         notesSubject.eraseToAnyPublisher()
     }
-    private let notesSubject = PassthroughSubject<[Note], Never>()
+    private let notesSubject: CurrentValueSubject<[Note], Never> = .init([])
+    private let notesCaretacer = NotesTableCaretacer(key: "Notes")
+    
+    init() {
+        notes = notesCaretacer.load()
+        notesSubject.send(notes)
+    }
     
     func createNote(note: Note) {
-        notes.append(note)
+        notes.insert(note, at: 0)
     }
     
     func editNote(for index: Int, note: Note) {
         notes[index] = note
+    }
+    
+    func deleteNote(for index: Int) {
+        notes.remove(at: index)
     }
 }
 
